2777d78f967bfd2e6db4b08d9aca68f6135befd4
AnwarKamel <anwarinfo24@gmail.com>
2022-03-27 21:18:37 +0100

    next

---
+++ README.md
@@ -0,0 +1,22 @@
+# Techo format Formation Project
+
+## Introduction
+Simple app, Formation android in Techno format School
+
+
+
+## Technologies used:
+* [Php Api](https://www.mulesoft.com/resources/api/what-is-an-api)
+* [Retrofit](https://square.github.io/retrofit/)
+* [OkHttp](https://square.github.io/okhttp/)
+* [Postman](https://www.postman.com/)
+* [MySQL](https://www.mysql.com/)
+
+
+## Screen
+
+<p>
+
+<img src="assets/1.PNG" alt="drawing" width="800"/>
+
+  </p>
--- app/src/main/java/com/example/technoformatprojects/activities/AboutActivity.kt
+++ app/src/main/java/com/example/technoformatprojects/activities/AboutActivity.kt
@@ -2,12 +2,30 @@ package com.example.technoformatprojects.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.technoformatprojects.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+
+       var test: CharSequence = "manel"
+
+        btn_show.setText("manel"  )
+
+        btn_show.setOnClickListener {
+
+        }
+
     }
+
+    fun show(view: View) {
+
+    }
+
+
 }
